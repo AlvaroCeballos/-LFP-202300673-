175bce2a036816ae423ecc836434e4235d9cbd8d
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2025-03-19 18:06:55 -0600

    Clase tokens

---
+++ Proyecto1/Proyecto1LFP/src/main/java/main/Tokens.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author aceba
+ */
+public class Tokens {
+    
+    private String lexema;
+    private String tipoToken;
+    private int posX;
+    private int posY;
+    
+    public Tokens(String lexema, String tipoToken, int posX, int posY){
+        this.lexema = lexema;
+        this.tipoToken = tipoToken;
+        this.posX = posX;
+        this.posY = posY;
+    }
+    
+}
