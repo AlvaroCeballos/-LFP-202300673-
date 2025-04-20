26ff34182cf7952836231109e509e2af4b695e34
AlvaroCeballos <a.ceballos.ivddb@gmail.com>
2025-04-19 23:52:45 -0600

    Se definen clases de error y token

--- Proyecto 2/Proyecto2LFP/src/main/java/com/mycompany/proyecto2lfp/ErrorSL.java
+++ Proyecto 2/Proyecto2LFP/src/main/java/com/mycompany/proyecto2lfp/ErrorSL.java
@@ -9,5 +9,39 @@ package com.mycompany.proyecto2lfp;
  * @author aceba
  */
 public class ErrorSL {
+    private String descripcion;
+    private String caracter;
+    private int posX;
+    private int posY;
+    private String tipoError;
     
+    public ErrorSL(String caracter, String descripcion, int posX, int posY, String tipoError){
+        this.caracter = caracter;
+        this.descripcion= descripcion;
+        this.posX = posX;
+        this.posY = posY;
+        this.tipoError = tipoError;
+    }
+    
+    @Override
+    public String toString(){
+        return( this.descripcion+" Linea: "+posX+" Columna: "+posY);
+    }
+    
+    
+    public String getCaracter() {
+        return caracter;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    public int getPosX() {
+        return posX;
+    }
+    
+    public int getPosY() {
+        return posY;
+    }
 }
--- Proyecto 2/Proyecto2LFP/src/main/java/com/mycompany/proyecto2lfp/Token.java
+++ Proyecto 2/Proyecto2LFP/src/main/java/com/mycompany/proyecto2lfp/Token.java
@@ -9,5 +9,76 @@ package com.mycompany.proyecto2lfp;
  * @author aceba
  */
 public class Token {
+        /**
+     * @return the lexema
+     */
+    public String getLexema() {
+        return lexema;
+    }
+
+    /**
+     * @param lexema the lexema to set
+     */
+    public void setLexema(String lexema) {
+        this.lexema = lexema;
+    }
+
+    /**
+     * @return the tipoToken
+     */
+    public String getTipoToken() {
+        return tipoToken;
+    }
+
+    /**
+     * @param tipoToken the tipoToken to set
+     */
+    public void setTipoToken(String tipoToken) {
+        this.tipoToken = tipoToken;
+    }
+
+    /**
+     * @return the posX
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * @param posX the posX to set
+     */
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    /**
+     * @return the posY
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * @param posY the posY to set
+     */
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
     
+    private String lexema;
+    private String tipoToken;
+    private int posX;
+    private int posY;
+    
+    public Token(String lexema, String tipoToken, int posX, int posY){
+        this.lexema = lexema;
+        this.tipoToken = tipoToken;
+        this.posX = posX;
+        this.posY = posY;
+    }
+    
+   @Override
+    public String toString(){
+        return("Token: '"+this.getTipoToken()+"'" +" Lexema: '"+this.getLexema()+"'" +" Linea: '"+getPosX()+"'" +" Columna: '" +getPosY()+"'");
+    }
 }
